44b0cfd46cd7e504832bacfc59e70f54447dc6d3
Pauli Rikula <pauli.rikula@gmail.com>
2026-03-14 00:11:36 +0200

    removed old debug prints

--- cluster/proxy/proxy_wrapper.py
+++ cluster/proxy/proxy_wrapper.py
@@ -3,11 +3,6 @@ import os
 # Import here so it sees the modified environment
 from litellm.proxy.proxy_cli import run_server
 
-print("env variables:")
-print('http_proxy', os.environ.get('http_proxy',   None) )
-print('https_proxy', os.environ.get('https_proxy', None) )
-print('HTTP_PROXY', os.environ.get('HTTP_PROXY',   None) )
-print('HTTPS_PROXY', os.environ.get('HTTPS_PROXY', None) )
 
 os.environ['http_proxy'] = ''
 os.environ['https_proxy'] = ''
